bf310281f518ed4d6bc1e6f3d7520deb06937c65
Corbin Hall <hallcorbin92@gmail.com>
2020-08-08 03:30:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var obj = {object: key}
-  delete object[key]
-  return object
+  
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
